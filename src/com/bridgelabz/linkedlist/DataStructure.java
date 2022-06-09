b59acb1b717c7436feb5757e91cfb202e676a1b1
namo-patil <namratapatil74738@gmail.com>
2022-06-09 19:11:27 +0530

    Added: Code to create simple linked list

---
+++ src/com/bridgelabz/linkedlist/DataStructure.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.linkedlist;
+
+public class DataStructure {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(70);
+        linkedList.add(30);
+        linkedList.add(56);
+
+        linkedList.print();
+    }
+}
---
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.linkedlist;
+
+public class LinkedList<T> {
+    Node<T> head;
+
+    public void add(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null){
+            head = newNode;
+        }
+        else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+    public void print(){
+        Node<T> currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + "--->");
+            currNode = currNode.next;
+        }
+        System.out.println("null");
+    }
+}
